5a2885dfabf0eb0db9ab0d72f5a3ab0ceed1c992
Ricardo Serrano <ricardo.serrano@softtek.com>
2020-04-16 13:34:35 -0500

    feat: Creando el POJO para los casos

---
+++ src/main/java/com/msacademy/mx/covid/model/Caso.java
@@ -0,0 +1,65 @@
+package com.msacademy.mx.covid.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Caso {
+
+    @Id
+    private Integer id;
+
+    @Column(name = "sector")
+    private Sector sector;
+
+    @Column(name = "entidad_unidad_medica")
+    private EntidadFederativa entidadUnidadMedica;
+
+    @Column(name = "sexo")
+    private Sexo sexo;
+
+    @Column(name = "entidad_nacimiento")
+    private EntidadFederativa entidadNacimiento;
+
+    @Column(name = "entidad_residencia")
+    private EntidadFederativa entidadResidencia;
+
+    @Column(name = "tipo_paciente")
+    private TipoPaciente tipoPaciente;
+
+    @Column(name = "fecha_ingreso")
+    private LocalDate fechaIngreso;
+
+    @Column(name = "fecha_inicio_sintomas")
+    private LocalDate fechaInicioSintomas;
+
+    @Column(name = "fecha_defuncion")
+    private LocalDate fechaDefuncion;
+
+    @Column(name = "intubado")
+    private Especificacion intubado;
+
+    @Column(name = "edad")
+    private Integer edad;
+
+    @Column(name = "nacionalidad")
+    private Nacionalidad nacionalidad;
+
+    @Column(name = "lengua_indigena")
+    private Especificacion lenguaIndigena;
+
+    @Column(name = "enfermedad")
+    private Enfermedad enfermedad;
+
+    @Column(name = "embarazo")
+    private Especificacion embarazo;
+
+    @Column(name = "resultado")
+    private Resultado resultado;
+
+    @Column(name = "cuidados_intensivos")
+    private Especificacion embarazo;
+
+}
