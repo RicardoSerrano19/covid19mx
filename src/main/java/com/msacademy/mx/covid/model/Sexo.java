e9b8c18c5c90aab8201d0fbc629477edcb2901ac
Ricardo Serrano <ricardo.serrano@softtek.com>
2020-04-16 14:06:46 -0500

    feat: Agregando lombok a los POJOS para automatizar la insercion de constructores, getters y setters

--- src/main/java/com/msacademy/mx/covid/model/Sexo.java
+++ src/main/java/com/msacademy/mx/covid/model/Sexo.java
@@ -1,11 +1,18 @@
 package com.msacademy.mx.covid.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "sexo")
 public class Sexo {
