28b1670929f40471b6ece11d189e02f4f29597e4
Ricardo Serrano <ricardo.serrano@softtek.com>
2020-04-22 13:35:11 -0500

    fix: Resolviendo problemas con los archivos y el component scan

--- src/main/java/com/msacademy/mx/covid/CovidApplication.java
+++ src/main/java/com/msacademy/mx/covid/CovidApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.msacademy.mx.covid"})
 public class CovidApplication {
 
 	public static void main(String[] args) {
